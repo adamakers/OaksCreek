b934d41ab0436ffd1f2dc230c58eb79e8bfa945d
adamakers <adamrakers@gmail.com>
2017-03-13 14:45:06 -0600

    added footer

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -20,15 +20,16 @@
 
 })();
 
-  function initMap() {
-    var uluru = {lat: 38.896139, lng: -104.850};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: uluru,
-      zoomControl: true
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
+function initMap() {
+  var uluru = {lat: 38.896139, lng: -104.850};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: uluru,
+    zoomControl: true
+  });
+  
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
