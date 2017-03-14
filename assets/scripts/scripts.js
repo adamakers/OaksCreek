113c843366cf919134d738c4361c66b97f67e05b
adamakers <adamrakers@gmail.com>
2017-03-14 12:08:57 -0600

    finished footer

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,35 +1,44 @@
-(function(){
 
-  function navbarScrolled(){
-    const topNav = document.querySelector('.navbar');
-    let scrollPos = window.scrollY;
+/******************************
+    PAGE FUNCTIONS
+******************************/
 
-    if (scrollPos > 60) {
-      topNav.classList.add('navbar-scrolled')
-    } else {
-      topNav.classList.remove('navbar-scrolled');
-    }
-  }
-
-  window.addEventListener('scroll', navbarScrolled);
-
-
-
-
-
-
-})();
-
+/******************************
+    GOOGLE MAPS
+******************************/
 function initMap() {
-  var uluru = {lat: 38.896139, lng: -104.850};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  const uluru = {lat: 38.896139, lng: -104.850};
+  let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: uluru,
-    zoomControl: true
+    zoomControl: true,
+    scrollwheel: false
   });
   
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
 }
+
+//Change header syling when scrolled, or onload
+function navbarScrolled(){
+  const topNav = document.querySelector('.navbar');
+  let scrollPos = window.scrollY;
+
+  if (scrollPos > 60) {
+    topNav.classList.add('navbar-scrolled')
+  } else {
+    topNav.classList.remove('navbar-scrolled');
+  }
+}
+
+
+
+/******************************
+    EVENT LISTENERS
+******************************/
+window.onload = navbarScrolled;
+window.addEventListener('scroll', navbarScrolled);
+
+
