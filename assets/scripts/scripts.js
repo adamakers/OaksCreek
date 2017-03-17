b769a97b9cd9348c5303a730bae71920eed4fabc
adamakers <adamrakers@gmail.com>
2017-03-16 18:17:32 -0600

    changed google map to static so that it would be responsive. Look at issue in next project

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,25 +3,6 @@
     PAGE FUNCTIONS
 ******************************/
 
-/******************************
-    GOOGLE MAPS
-******************************/
-
-function initMap() {
-  const uluru = {lat: 38.896139, lng: -104.850};
-  let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: uluru,
-    zoomControl: true,
-    scrollwheel: false
-  });
-  
-  let marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
-
 //Change header syling when scrolled, or onload
 function navbarScrolled(){
   const topNav = document.querySelector('.navbar');
